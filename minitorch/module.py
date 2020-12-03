0c7c0c3f8f83c74ba20e9444079f1c8957493b10
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2020-12-03 18:26:35 +0530

    added mod0 changes

--- minitorch/module.py
+++ minitorch/module.py
@@ -22,11 +22,15 @@ class Module:
 
     def train(self):
         "Set the mode of this module and all descendent modules to `train`."
-        raise NotImplementedError('Need to include this file from past assignment.')
+        self.mode = "train"
+        for md in self.modules():
+            md.train()
 
     def eval(self):
         "Set the mode of this module and all descendent modules to `eval`."
-        raise NotImplementedError('Need to include this file from past assignment.')
+        self.mode = "eval"
+        for md in self.modules():
+            md.eval()
 
     def named_parameters(self):
         """
@@ -36,7 +40,11 @@ class Module:
         Returns:
             dict: Each name (key) and :class:`Parameter` (value) under this module.
         """
-        raise NotImplementedError('Need to include this file from past assignment.')
+        np = dict(self.__dict__["_parameters"])
+        for md_name, md in self.__dict__["_modules"].items():
+            for n, p in md.named_parameters().items():
+                np.update({f"{md_name}.{n}": p})
+        return np
 
     def parameters(self):
         return self.named_parameters().values()
